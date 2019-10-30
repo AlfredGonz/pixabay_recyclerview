d9fd172fa3e3d8edf63db5b649c183bbb30c5e59
Alfredo Gonzalez <manuel.martinez96@hotmail.com>
2019-10-30 11:14:06 -0600

    creating function ParsedJSON

--- app/src/main/java/com/example/pixabay_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/pixabay_recyclerview/MainActivity.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -34,5 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void ParseJSON(){
+        String URL = "https://pixabay.com/api/?key=13820626-6d25cdf74b1dc69b8dbda10fa&q=paisajes&image_type=photo&pretty=true";
 
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
 }
